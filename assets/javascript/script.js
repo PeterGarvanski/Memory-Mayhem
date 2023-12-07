1f4f489c910b2ba2ae1a3066c08b04104d907a73
Peter Garvanski <garvanski.peter@gmail.com>
2023-12-07 16:47:06 +0000

    feat: fixed bugs and made it oop

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,43 +1,60 @@
-function gameSequence() {
-    let game = {
-        level: 3,
-        score: 0,
-        highScore: 0,
-        generatedSquares: [],
-        selectedSquares: [],
-        lives: 0
-    };
-
-    startGame();
-    displayBuilder(game.level);
-}
-
-$("#start-button").click(gameSequence);
-
-function startGame() {
-    $("#score-card-box").siblings().remove();
-    $(".game-display").css("background-color", "white");
-}
-
-function displayBuilder(level) {
-    let startHtml = `<div class="center center-box"><div class="row box-display">`;
-    let box = `<div class="box shadow"></div>`;
-    let endHtml = `</div></div>`;
-    let divAmount = ``;
-
-    // Use the level number to generate the required amount of boxes
-    for (let i = 0; i < level * level; i++) {
-        divAmount += box;
+class Game {
+    
+    constructor(score, highScore, level, lives) {
+      this.score = score;
+      this.highScore = highScore;
+      this.level = level
+      this.lives = lives
+      this.generatedSquares = []
+      this.selectedSquares = []
     }
 
-    // Clear existing content and append the generated divs to the display container
-    let scoreCard = $("#score-card-box");
-    scoreCard.after(startHtml); // Add the start HTML
+    getScore() {
+        return this.score
+    }
 
-    let boxDisplay = $(".box-display");
-    boxDisplay.css("grid-template-columns", "repeat(" + level + ", 1fr)");
-    boxDisplay.css("grid-template-rows", "repeat(" + level + ", 1fr)");
-    boxDisplay.append(divAmount); // Append the generated divs
-    boxDisplay.after(endHtml); // Add the end HTML
+    getHighScore() {
+        return this.highScore
+    }
+
+    getLevel() {
+        return this.level
+    }
+
+    getLives() {
+        return this.lives
+    }
+
+    startGame() {
+        $("#score-card-box").siblings().remove();
+        $(".game-display").css("background-color", "white");
+    }
+    
+    displayBuilder(level) {
+        let startHtml = `<div class="center center-box"><div class="row box-display"></div></div>`;
+        let box = `<div class="box shadow"></div>`;
+        
+        // Clear existing content and append the start HTML
+        let scoreCard = $("#score-card-box");
+        scoreCard.after(startHtml);
+        
+        let boxDisplay = $(".box-display");
+        boxDisplay.css("grid-template-columns", `repeat(${level}, 1fr)`);
+        boxDisplay.css("grid-template-rows", `repeat(${level}, 1fr)`);
+    
+        // Use the level number to generate the required amount of boxes
+        for (let i = 0; i < level * level; i++) {
+            boxDisplay.append(box); // Append each box as a child of boxDisplay
+        }
+    }
+    
 }
-
+    
+function gameSequence() {
+    const user = new Game(0, 0, 3, 3);
+    user.startGame();
+    user.displayBuilder(user.getLevel());
+}
+    
+$("#start-button").click(gameSequence);
+    
