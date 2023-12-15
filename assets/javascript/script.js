3253b27e903463604e5a00f1dd113aa9cc463ee9
Peter Garvanski <garvanski.peter@gmail.com>
2023-12-15 15:04:44 +0000

    fix: commented testing

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
 // Uncomment the line below for your Tests to run
-const $ = require('jquery');
+// const $ = require('jquery');
 
 class Game {
     constructor(score, level, lives) {
@@ -251,4 +251,4 @@ async function gameSequence() {
 $(document).on("click", "#start-button", gameSequence);
 
 // Uncomment the line below for Tests to run
-module.exports = { Game };
+// module.exports = { Game };
